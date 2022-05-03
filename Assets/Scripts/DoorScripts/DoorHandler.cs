95cb955dc2aee18818de6f0a5188f0f515efbf7c
egemenengin <58360345+egemenengin@users.noreply.github.com>
2022-05-03 19:22:02 +0300

    Door Prefabs are Added

--- Assets/Scripts/DoorScripts/DoorHandler.cs
+++ Assets/Scripts/DoorScripts/DoorHandler.cs
@@ -6,11 +6,14 @@ public class DoorHandler : MonoBehaviour
 {
     [SerializeField]private DoorType doorType;
     public string DoorColor;
-    
+
+    public GameObject Door;
+
+    public Transform Hinge;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OpeningDoor();
     }
 
     // Update is called once per frame
@@ -24,6 +27,7 @@ public class DoorHandler : MonoBehaviour
         if (doorType == DoorType.Key)
         {
             //If player has a right key, open.
+            OpeningDoor();
         }
     }
     public void OpenPasswordDoor()
@@ -31,6 +35,7 @@ public class DoorHandler : MonoBehaviour
         if (doorType == DoorType.Password)
         {
             //If player found the password, open.
+            OpeningDoor();
         }
       
     }
@@ -46,6 +51,7 @@ public class DoorHandler : MonoBehaviour
         if (doorType == DoorType.Lever)
         {
             //If player open a lever, open
+            OpeningDoor();
         }
     }
     public void OpenPressurePlateDoor()
@@ -53,8 +59,13 @@ public class DoorHandler : MonoBehaviour
         if (doorType == DoorType.PressurPlate)
         {
             //If player press the pressure plate, open.
+            OpeningDoor();
         }
         
     }
-    
+
+    public void OpeningDoor()
+    {
+        Door.transform.RotateAround(Hinge.position,new Vector3(0,1,0),-90f);
+    }
 }
