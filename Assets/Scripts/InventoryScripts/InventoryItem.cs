4a526b31843340c05a84a08f5717056dbe21a8e8
baris-sinapli <43214847+baris-sinapli@users.noreply.github.com>
2022-05-06 20:23:01 +0300

    Collectible inventory item constructor is created

---
+++ Assets/Scripts/InventoryScripts/InventoryItem.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InventoryItem : MonoBehaviour
+{
+    public Item item;
+
+    public void OnPickup()
+    {
+        //gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+}
+public class InventoryEventArgs : EventArgs
+{
+    public InventoryEventArgs(InventoryItem item)
+    {
+        Item = item;
+    }
+    public InventoryItem Item;
+}
+
+
+
