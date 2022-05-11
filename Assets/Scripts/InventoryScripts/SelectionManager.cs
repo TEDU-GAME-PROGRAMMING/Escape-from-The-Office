9ebc1ddae0fad9e99bf80a0eb128213f52ec1889
baris-sinapli <43214847+baris-sinapli@users.noreply.github.com>
2022-05-11 20:00:18 +0300

    Holding system added and prefabs edited

--- Assets/Scripts/InventoryScripts/SelectionManager.cs
+++ Assets/Scripts/InventoryScripts/SelectionManager.cs
@@ -1,63 +1,108 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
     [SerializeField]
-    float maxDistance = 5f;
+    float maxDistance = 0.9f;
 
     public Inventory inventory;
+    public GameObject interact;
     public TextMeshProUGUI interactTextMesh;
+    public TextMeshProUGUI keyboardButtonName;
+    public Image keyboardButtonProgress;
+    public float interactDuration;
+    public float collectDuration;
 
+    private float currentTimeElapsed;
+    private bool isFinished = false;
 
     void Update()
     {
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, maxDistance))
+        if (Physics.Raycast(ray, out hit, maxDistance))
         {
             var selection = hit.transform;
-            if(selection.GetComponent<InventoryItem>() != null)
+            if (selection.GetComponent<InventoryItem>() != null)
             {
                 InventoryItem inventoryItem = selection.GetComponent<InventoryItem>();
-                
-                interactTextMesh.enabled = true;
-                interactTextMesh.text = $"Press [E] to pick up <color=\"red\"><b>{inventoryItem.item.itemName}</b></color>";
-                
+
+                interact.SetActive(true);
+                interactTextMesh.text = $"Hold down [E] to pick up <color=\"red\"><b>{inventoryItem.item.itemName}</b></color>";
+                keyboardButtonName.text = "E";
 
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    inventory.AddItem(inventoryItem);
-                }
-                    
-            } 
-
-            if(selection.GetComponent<Lever>() != null)
-            {
-                Lever lever = selection.GetComponent<Lever>();
-                interactTextMesh.enabled = true;
-
-                if (lever.isActive)
-                {
-                    interactTextMesh.text = "Press [E] to <color=\"red\"><b>disable</b></color> lever";
+                    IncrementProgress(collectDuration);
+                    if (isFinished)
+                    {
+                        inventory.AddItem(inventoryItem);
+                        isFinished = false;
+                    }
                 }
                 else
                 {
-                    interactTextMesh.text = "Press [E] to <color=\"red\"><b>enable</b></color> lever";
+                    currentTimeElapsed = 0f;
                 }
-
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    lever.isActive = !lever.isActive;
-                }
+                updateProgressImage(collectDuration);
             }
-            
+
+            if (selection.GetComponent<Lever>() != null)
+            {
+                Lever lever = selection.GetComponent<Lever>();
+                interact.SetActive(true);
+
+                if (lever.isActive)
+                {
+                    interactTextMesh.text = "Hold down [E] to <color=\"red\"><b>disable</b></color> lever";
+                }
+                else
+                {
+                    interactTextMesh.text = "Hold down [E] to <color=\"red\"><b>enable</b></color> lever";
+                }
+                keyboardButtonName.text = "E";
+
+                if (Input.GetKey(KeyCode.E))
+                {
+                    IncrementProgress(interactDuration);
+                    if (isFinished)
+                    {
+                        lever.isActive = !lever.isActive;
+                        isFinished = false;
+                    }
+                }
+                else
+                {
+                    currentTimeElapsed = 0f;
+                }
+                updateProgressImage(interactDuration);
+            }
+
         }
         else
         {
-            interactTextMesh.enabled = false;
+            interact.SetActive(false);
+        }
+    }
+
+    private void updateProgressImage(float duration)
+    {
+        float ppd = currentTimeElapsed / duration;
+        keyboardButtonProgress.fillAmount = ppd;
+    }
+
+    private void IncrementProgress(float duration)
+    {
+        currentTimeElapsed += Time.deltaTime;
+        if (currentTimeElapsed >= duration)
+        {
+            isFinished = true;
+            currentTimeElapsed = 0f;
         }
     }
 }
