d0f6c7269603830b292861e0fd1585853e2ed238
egemenengin <58360345+egemenengin@users.noreply.github.com>
2022-05-11 20:12:32 +0300

    BinaryLeverDoorSystem added & Level designs in Level with name "Level Source"

---
+++ Assets/Scripts/DoorScripts/BinaryLeverDoor.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryLeverDoor : MonoBehaviour
+{
+    public List<Lever> Levers;
+    public int Answer;
+    public string BinaryAnswer;
+    public List<bool> TrueLeverCount;
+    
+    public DoorHandler LeverDoor;
+
+    public bool isOpened = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BinaryAnswer = System.Convert.ToString(Answer, 2);
+        Debug.Log(BinaryAnswer + " " + Levers.Count);
+        //TrueLeverCount = new List<int>(BinaryAnswer.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        LeverAnswerCheck();
+        
+        
+    }
+
+    public void LeverAnswerCheck()
+    {
+        for (int i = 0; i < BinaryAnswer.Length; i++)
+        {
+            
+            if (BinaryAnswer[i] == '1')
+            {
+               
+                if (Levers[i].isActive)
+                {
+                    TrueLeverCount[i] = true;
+                    Debug.Log("1: " + i);
+                }
+                else
+                {
+                    TrueLeverCount[i] = false;
+                }
+              
+            }
+            else
+            {
+                
+                if (!Levers[i].isActive)
+                {
+                    
+                    TrueLeverCount[i] = true;
+                    Debug.Log("0: " + i);
+                }
+                else
+                {
+                    TrueLeverCount[i] = false;
+                }
+                
+            }
+        }
+
+        if (!TrueLeverCount.Contains(false) && !isOpened)
+        {
+            Debug.Log("BURADA ACİLDİ");
+            LeverDoor.OpenLeverDoor();
+            isOpened = true;
+        }
+    }
+}
